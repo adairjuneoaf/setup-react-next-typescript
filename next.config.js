41340d924728949be5849be567a7071af24b4aa6
Adair Juneo <adair_juneo@hotmail.com>
2022-01-05 00:37:24 -0300

    Commit final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+
+const withImages = require('next-images')
+
+module.exports = withImages({
+  esModule: true,
+  reactStrictMode: true
+})
